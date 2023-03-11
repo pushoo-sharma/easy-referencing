d8b3ee543c204896898dcd513cde53639a4fae81
Pushpak Sharma <pushpaksharma@Pushpaks-MacBook-Air.local>
2023-03-12 03:49:05 +0530

    Dynamic Inputs in React

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-// import ChildFunc from './child';
 
 function App() {
 
-  const age = 22;
+  const [inputs, setInputs] = useState({});
+
+  const getInputs = (data) => {
+    const {name, value} = data.target;
+    let newInputs = {[name]: value};
+    setInputs({...inputs,...newInputs});
+  }
 
   return (
     <div className="App">
-      {
-        age && age > 18 ? (<><div>check</div> {console.log("checking ")}</>) : (<div></div>)
-      }
+      <div>
+        <input placeholder="name" name="name" onChange={getInputs}/>
+        <input placeholder="surname" name="surname" onChange={getInputs}/>
+      </div>
     </div>
   );
 }
